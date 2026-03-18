f8872da5b103342492137a391d81755db2ae01a3
Natalia Mahecha <zharik.mahecha-c@mail.escuelaing.edu.co>
2026-03-17 23:13:15 -0500

    Se agrego la parte de core

--- src/main/java/edu/eci/dosw/tdd/core/model/Book.java
+++ src/main/java/edu/eci/dosw/tdd/core/model/Book.java
@@ -1,7 +1,17 @@
 package edu.eci.dosw.tdd.core.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class Book {
-    private long id;
+    private String id;
     private String title;
     private String author;
 }
